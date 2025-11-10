bb04a250c0cc3821a6850b7896236209ae2d9eda
Pereirapedr0 <pmateus1022@gmail.com>
2025-11-10 10:17:44 -0300

    Add implementation of mostrarHora function with setInterval and setTimeout for time logging

---
+++ Javascript/setInterval, steTimeout/index.js
@@ -0,0 +1,14 @@
+function mostrarHora() {
+  let data = new Date();
+    return data.toLocaleTimeString("pt-BR", {
+        hour12: false
+    });
+}
+
+const timer = setInterval(function() {
+    console.log(mostrarHora());
+}, 1000);
+
+setTimeout(function() {
+    clearInterval(timer);
+}, 5000);
